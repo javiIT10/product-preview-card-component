8f7e53c087ed842f33630096d54f9c3daedcbe30
Javier <javier.duarte.iti@gmail.com>
2024-07-30 14:14:58 -0500

    Añadiendo Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}", "./src/js/app.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        fraunces: ["Fraunces", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      colors: {
+        /* ### Primary */
+        "dark-cyan": "hsl(158, 36%, 37%)",
+        cream: "hsl(30, 38%, 92%)",
+
+        /* ### Neutral */
+        "very-dark-blue": "hsl(212, 21%, 14%)",
+        "dark-grayish-blue": "hsl(228, 12%, 48%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
